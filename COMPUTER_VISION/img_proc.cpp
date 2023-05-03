63183db6fd5d213e5d0552b7603288d3c025c091
BONCHEUL KOO <goobncl@gmail.com>
2023-05-03 11:24:27 +0400

    코드 정리

--- COMPUTER_VISION/img_proc.cpp
+++ COMPUTER_VISION/img_proc.cpp
@@ -1,12 +1,11 @@
 #include "stdafx.h"
 #include "img_proc.h"
 
-void invertColors(unsigned char* input, int width, int height)
-{
+void invertColors(unsigned char* input, int width, int height) {
+
 	qDebug() << "[1] -*- Start on thread ID:" << QThread::currentThreadId();
 
-	for (int i = 0; i < width * height; ++i)
-	{
+	for (int i = 0; i < width * height; ++i) {
 		input[i] = 255 - input[i];
 	}
 
