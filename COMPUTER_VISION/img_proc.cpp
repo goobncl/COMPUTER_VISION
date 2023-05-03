abf2409446fd5bf3536b4c5f204f79c7ae210b44
BONCHEUL KOO <goobncl@gmail.com>
2023-05-03 12:49:07 +0400

    주석제거

--- COMPUTER_VISION/img_proc.cpp
+++ COMPUTER_VISION/img_proc.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "img_proc.h"
 
+
 void invertColors(unsigned char* input, int width, int height) {
 
 	for (int i = 0; i < width * height; ++i) {
@@ -13,32 +14,30 @@ void histogramEqualization(unsigned char* input, int width, int height) {
     int size = width * height;
     int num_bins = 256;
 
-    // Calculate the histogram
     int histogram[256] = { 0 };
     for (int i = 0; i < size; ++i) {
         histogram[input[i]]++;
     }
 
-    // Calculate the cumulative distribution function (CDF)
     int cdf[256] = { 0 };
     cdf[0] = histogram[0];
     for (int i = 1; i < num_bins; ++i) {
         cdf[i] = cdf[i - 1] + histogram[i];
     }
 
-    // Find the minimum and maximum values in the CDF
     int cdf_min = INT_MAX;
     int cdf_max = INT_MIN;
     for (int i = 0; i < num_bins; ++i) {
+        
         if (cdf[i] < cdf_min) {
             cdf_min = cdf[i];
         }
+
         if (cdf[i] > cdf_max) {
             cdf_max = cdf[i];
         }
     }
 
-    // Normalize the CDF and apply the mapping to the input image
     int cdf_range = cdf_max - cdf_min;
     for (int i = 0; i < size; ++i) {
         input[i] = (unsigned char)(((cdf[input[i]] - cdf_min) * 255) / cdf_range);
