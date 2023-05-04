dc227dc604efb3e19f054eca448022f96d94581e
BONCHEUL KOO <goobncl@gmail.com>
2023-05-04 16:36:03 +0400

    상수 정리

--- COMPUTER_VISION/CLAHE.cpp
+++ COMPUTER_VISION/CLAHE.cpp
@@ -1,7 +1,13 @@
 #include "stdafx.h"
 #include "CLAHE.h"
 
-void computeHistogram(unsigned char* input, int width, int subWidth, int subHeight, int row, int col, int histogram[256]) {
+#define GRAY_LVL 256
+#define SUB_W 8
+#define SUB_H 6
+#define ALPHA 0.0001
+
+
+void computeHistogram(unsigned char* input, int width, int subWidth, int subHeight, int row, int col, int histogram[GRAY_LVL]) {
 
     for (int i = row * subHeight; i < (row + 1) * subHeight; ++i) {
         for (int j = col * subWidth; j < (col + 1) * subWidth; ++j) {
@@ -11,10 +17,10 @@ void computeHistogram(unsigned char* input, int width, int subWidth, int subHeig
     }
 }
 
-void redistributeExcess(int histogram[256], int num_bins, int clipValue) {
+void redistributeExcess(int histogram[GRAY_LVL], int clipValue) {
 
     int excess = 0;
-    for (int i = 0; i < num_bins; ++i) {
+    for (int i = 0; i < GRAY_LVL; ++i) {
         int excess_value = histogram[i] - clipValue;
         if (excess_value > 0) {
             excess += excess_value;
@@ -22,18 +28,18 @@ void redistributeExcess(int histogram[256], int num_bins, int clipValue) {
         }
     }
 
-    int redistribute = excess / num_bins;
-    for (int i = 0; i < num_bins; ++i) {
+    int redistribute = excess / GRAY_LVL;
+    for (int i = 0; i < GRAY_LVL; ++i) {
         histogram[i] += redistribute;
     }
 }
 
-void computeCDF(int histogram[256], int cdf[256], int num_bins, int* cdf_min, int* cdf_max) {
+void computeCDF(int histogram[GRAY_LVL], int cdf[GRAY_LVL], int* cdf_min, int* cdf_max) {
 
     cdf[0] = histogram[0];
     *cdf_min = cdf[0];
     *cdf_max = cdf[0];
-    for (int i = 1; i < num_bins; ++i) {
+    for (int i = 1; i < GRAY_LVL; ++i) {
         cdf[i] = cdf[i - 1] + histogram[i];
         if (cdf[i] < *cdf_min) {
             *cdf_min = cdf[i];
@@ -44,7 +50,7 @@ void computeCDF(int histogram[256], int cdf[256], int num_bins, int* cdf_min, in
     }
 }
 
-void applyMapping(unsigned char* input, int width, int subWidth, int subHeight, int row, int col, int cdf[256], int cdf_min, int cdf_range) {
+void applyMapping(unsigned char* input, int width, int subWidth, int subHeight, int row, int col, int cdf[GRAY_LVL], int cdf_min, int cdf_range) {
 
     for (int i = row * subHeight; i < (row + 1) * subHeight; ++i) {
         for (int j = col * subWidth; j < (col + 1) * subWidth; ++j) {
@@ -56,30 +62,28 @@ void applyMapping(unsigned char* input, int width, int subWidth, int subHeight, 
 void gridHistogramEqualization(unsigned char* input, int width, int height) {
 
     int size = width * height;
-    int num_bins = 256;
-    int subWidth = width / 8;
-    int subHeight = height / 6;
-    double alpha = 0.0001;
-    int mapping_functions[8][6][256] = { 0 };
+    int subWidth = width / SUB_W;
+    int subHeight = height / SUB_H;
+    int mapping_functions[SUB_W][SUB_H][GRAY_LVL] = { 0 };
 
-    for (int row = 0; row < 6; ++row) {
-        for (int col = 0; col < 8; ++col) {
+    for (int row = 0; row < SUB_H; ++row) {
+        for (int col = 0; col < SUB_W; ++col) {
 
-            int histogram[256] = { 0 };
+            int histogram[GRAY_LVL] = { 0 };
             int Nb = subWidth * subHeight;
-            int L = num_bins;
-            int clipValue = (int)((Nb / L) + alpha * (Nb - Nb / L));
-            int cdf[256] = { 0 };
+            int L = GRAY_LVL;
+            int clipValue = (int)((Nb / L) + ALPHA * (Nb - Nb / L));
+            int cdf[GRAY_LVL] = { 0 };
             int cdf_min, cdf_max;
 
             computeHistogram(input, width, subWidth, subHeight, row, col, histogram);
-            redistributeExcess(histogram, num_bins, clipValue);
-            computeCDF(histogram, cdf, num_bins, &cdf_min, &cdf_max);
+            redistributeExcess(histogram, clipValue);
+            computeCDF(histogram, cdf, &cdf_min, &cdf_max);
 
             int cdf_range = cdf_max - cdf_min;
             applyMapping(input, width, subWidth, subHeight, row, col, cdf, cdf_min, cdf_range);
 
-            for (int i = 0; i < num_bins; ++i) {
+            for (int i = 0; i < GRAY_LVL; ++i) {
                 mapping_functions[row][col][i] = ((cdf[i] - cdf_min) * 255) / cdf_range;
             }
         }
